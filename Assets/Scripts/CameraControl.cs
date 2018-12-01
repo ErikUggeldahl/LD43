9c33cd36685c68b2bedd5643d8e805a82a6821fd
Erik Uggeldahl <erikuggeldahl@gmail.com>
2018-11-30 21:38:06 -0500

    Added camera control

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    const float MOVE_SPEED = 15f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * MOVE_SPEED * Time.deltaTime;
+        transform.Translate(move, Space.World);
+    }
+}
