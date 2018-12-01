8339c64d9b096009769aef44416e48874c48a1bf
Erik Uggeldahl <erikuggeldahl@gmail.com>
2018-12-01 00:08:26 -0500

    Added rudimentary farm building

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public Picker picker;
+
+    public GameObject farm;
+
+    enum State
+    {
+        Idle,
+        BuildSelect,
+        Picking,
+    }
+    State state;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (state == State.Idle)
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                StartBuilding();
+            }
+        }
+        else if (state == State.BuildSelect)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                state = State.Idle;
+            }
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                var farm = Instantiate(this.farm);
+                farm.name = "Farm";
+                StartPicking(farm);
+            }
+        }
+        else if (state == State.Picking)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                StopPicking();
+            }
+        }
+    }
+
+    void StartBuilding()
+    {
+        state = State.BuildSelect;
+    }
+
+    void StartPicking(GameObject building)
+    {
+        state = State.Picking;
+        picker.StartPicking(building, FinishPicking);
+    }
+
+    void FinishPicking(GameObject building)
+    {
+        state = State.Idle;
+    }
+
+    void StopPicking()
+    {
+        state = State.Idle;
+        picker.StopPicking();
+    }
+}
---
+++ Assets/Scripts/Picker.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Picker : MonoBehaviour
+{
+    public GameObject nodes;
+    public Camera cam;
+    public Transform blueprint;
+
+    GameObject building = null;
+    Action<GameObject> finished;
+    float pickingRadius = 0.0f;
+
+    int groundMask;
+
+    void Start()
+    {
+        groundMask = LayerMask.GetMask("Ground");
+    }
+
+    public void StartPicking(GameObject building, Action<GameObject> finished)
+    {
+        enabled = true;
+
+        this.building = building;
+        this.finished = finished;
+
+        building.transform.parent = blueprint;
+        building.transform.localPosition = Vector3.zero;
+
+        pickingRadius = building.GetComponent<SphereCollider>().radius;
+
+        blueprint.gameObject.SetActive(true);
+        blueprint.GetChild(0).localScale = new Vector3(pickingRadius, pickingRadius, pickingRadius);
+    }
+
+    public void StopPicking()
+    {
+        Destroy(this.building);
+        this.building = null;
+        blueprint.gameObject.SetActive(false);
+        enabled = false;
+    }
+
+    void FinishPicking()
+    {
+        finished(this.building);
+        this.building = null;
+        blueprint.gameObject.SetActive(false);
+        enabled = false;
+    }
+
+    void Update()
+    {
+        var colliders = nodes.GetComponentsInChildren<SphereCollider>();
+
+        RaycastHit hit;
+        var ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, float.PositiveInfinity, groundMask))
+        {
+            foreach (var collider in colliders)
+            {
+                if (Vector3.Distance(collider.transform.position, hit.point) < collider.radius + pickingRadius) return;
+            }
+            blueprint.position = hit.point;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            building.transform.parent = nodes.transform;
+            FinishPicking();
+        }
+    }
+}
