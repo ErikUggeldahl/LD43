8db848b7e77d494afc6a792ac6781c1ef27ebbe7
Erik Uggeldahl <erikuggeldahl@gmail.com>
2018-12-02 14:34:35 -0500

    Added bounds to the camera

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -14,5 +14,10 @@ public class CameraControl : MonoBehaviour
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * MOVE_SPEED * Time.deltaTime;
         transform.Translate(move, Space.World);
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, -100f, 100f),
+            transform.position.y,
+            Mathf.Clamp(transform.position.z, -110f, 110f)
+        );
     }
 }
