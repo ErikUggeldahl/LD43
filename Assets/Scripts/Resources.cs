3689a1b3ed22b54e85d5e1499c7da48cbbf7a068
Erik Uggeldahl <erikuggeldahl@gmail.com>
2018-12-02 19:29:09 -0500

    Added win/loss message

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -3,13 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Resources : MonoBehaviour
 {
     public RectTransform sacrificeBar;
     public Text coinText;
 
+    public GameObject resultPanel;
+    public TextMeshProUGUI resultText;
+
     public event Action CoinChanged;
+
+    public int owner;
+
+    bool gameFinished = false;
 
     int sacrifices = 0;
     public int Sacrifices { get { return sacrifices; } }
@@ -20,14 +28,34 @@ public class Resources : MonoBehaviour
 
     void Start()
 	{
+        resultPanel.SetActive(false);
+
         sacrificeBar.localScale = new Vector3(Mathf.Min((float)sacrifices / TOTAL_SACRIFICES, 1f), 1f, 1f);
         if (coinText) coinText.text = coins.ToString();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            resultPanel.SetActive(false);
+        }
     }
 
     public void AddSacrifice(int value)
     {
         sacrifices += value;
         sacrificeBar.localScale = new Vector3(Mathf.Min((float)sacrifices / TOTAL_SACRIFICES, 1f), 1f, 1f);
+
+        if (sacrifices >= TOTAL_SACRIFICES && !gameFinished)
+        {
+            gameFinished = true;
+            resultPanel.SetActive(true);
+            if (owner != 0)
+            {
+                resultText.text = "YOU LOSE";
+            }
+        }
     }
 
     public void AddCoin(int value)
